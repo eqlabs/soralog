fb0320e3a711d55f31321f447ef51bfb3ba98bce
Dmytro Gordon <g-d@ukr.net>
2022-09-01 15:47:57 +0300

    Add Conan package manager support

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,15 +4,12 @@
 #
 
 if (TESTING OR COVERAGE)
-  hunter_add_package(GTest)
-  find_package(GTest CONFIG REQUIRED)
+  add_3rdparty(GTest)
 endif()
 
-hunter_add_package(yaml-cpp)
-find_package(yaml-cpp CONFIG REQUIRED)
+add_3rdparty(yaml-cpp)
 if (NOT TARGET yaml-cpp::yaml-cpp)
     add_library(yaml-cpp::yaml-cpp ALIAS yaml-cpp)
 endif()
 
-hunter_add_package(fmt)
-find_package(fmt CONFIG REQUIRED)
+add_3rdparty(fmt)
