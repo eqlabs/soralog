f240aa6728a9b9e2c669cfe3b23b981c2c74c5d4
Vaclav Barta <vbarta@mangrove.cz>
2022-09-02 10:03:41 +0200

    added test_package

---
+++ test_package/conanfile.py
@@ -0,0 +1,13 @@
+from conans import ConanFile, CMake
+
+class SoralogTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        self.run("./packagetest")
