f989a8c9618d7789d907f7745987a40b1da9d760
Vaclav Barta <vbarta@mangrove.cz>
2022-09-07 14:09:28 +0200

    basic conanfile.py (#2)
    * basic conanfile.py
    
    * extended to actually support conan create
    
    * added test_package
    
    * fixed test_package, by installing into standard libdir
    
    * libdir conditional on package manager
    
    * Fix test package building
    
    * Move all conan-related things to a separate folder
    
    * removed top-level test_package
    
    * trying soralog example in test package
    
    * Revert "trying soralog example in test package"
    
    This reverts commit 287200272f1d83c9e85ef054f1849d9faa5d9af0.
    
    Co-authored-by: Dmytro Gordon <g-d@ukr.net>

---
+++ conan/conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, CMake
+
+class SoralogConan(ConanFile):
+   name = "soralog"
+   version = "0.0.7"
+   settings = "os", "compiler", "build_type", "arch"
+   requires = "fmt/6.1.2", "yaml-cpp/0.7.0"
+   generators = "cmake"
+   # We don't use '/' after '..' as a workaround for
+   # https://github.com/conan-io/conan/issues/2675 issue
+   exports_sources = "../CMakeLists.txt", "../cmake*", "../example*", "../include*", "../src*", "../test*"
+
+   def build(self):
+      cmake = self._configure_cmake()
+      cmake.build()
+
+   def package(self):
+      cmake = self._configure_cmake()
+      cmake.install()
+
+   def _configure_cmake(self):
+      cmake = CMake(self)
+      cmake.definitions["PACKAGE_MANAGER"] = "Conan"
+      cmake.configure()
+      return cmake
+
+   def package_info(self):
+      self.cpp_info.libdirs = ["lib/soralog"]
+      self.cpp_info.libs = ["soralog"]
